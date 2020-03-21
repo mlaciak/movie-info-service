2e1365e981ecfadaea8244f7bfb39c1a16f72e11
Michał Łaciak <laciak.michal@gmail.com>
2020-03-21 17:31:54 +0100

    Spring Boot Microservices Level 2: Fault Tolerance and Resilience

--- src/main/java/com/example/movieinfoservice/models/Movie.java
+++ src/main/java/com/example/movieinfoservice/models/Movie.java
@@ -3,13 +3,15 @@ package com.example.movieinfoservice.models;
 public class Movie {
     private String movieId;
     private String name;
+    private String description;
 
     public Movie() {
     }
 
-    public Movie(String movieId, String name) {
+    public Movie(String movieId, String name, String description) {
         this.movieId = movieId;
         this.name = name;
+        this.description = description;
     }
 
     public String getMovieId() {
@@ -26,5 +28,22 @@ public class Movie {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId='" + movieId + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
